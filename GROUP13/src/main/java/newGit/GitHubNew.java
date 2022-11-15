cc1ff570be6e95804dabac96e896fe3da72a377a
Abc <Abc@DESKTOP-038U14I>
2022-11-15 18:31:21 +0530

    First Commit

---
+++ GROUP13/src/main/java/newGit/GitHubNew.java
@@ -0,0 +1,19 @@
+package newGit;
+
+public class GitHubNew {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+		
+	}
+
+}
