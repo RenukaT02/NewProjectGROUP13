d1479a315df4ed0c1f582e59d94b7a9466359c94
RenukaT02 <118275410+RenukaT02@users.noreply.github.com>
2022-11-15 19:27:00 +0530

    Update GitHubNew.java

--- GROUP13/src/main/java/newGit/GitHubNew.java
+++ GROUP13/src/main/java/newGit/GitHubNew.java
@@ -4,11 +4,7 @@ public class GitHubNew {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Github");
-		System.out.println("Github");
-		System.out.println("Github");
-		System.out.println("Github");
-		System.out.println("Github");
+		
 		System.out.println("Github");
 		System.out.println("Github");
 		System.out.println("Github");
